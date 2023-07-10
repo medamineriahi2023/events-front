ed79df217b532b4c0765c6f9a25b65e78a25f1b5
sinda.louil <Sinda.Louil@pyxis.com.tn>
2023-07-10 22:33:41 +0100

    Start develop forum section

--- src/app/modules/participant/publications/publications.module.ts
+++ src/app/modules/participant/publications/publications.module.ts
@@ -5,7 +5,7 @@ import { PublicationsRoutingModule } from './publications-routing.module';
 import { AddPublicationComponent } from './add-publication/add-publication.component';
 import { ListPublicationComponent } from './list-publication/list-publication.component';
 import { PublicationDetailsComponent } from './publication-details/publication-details.component';
-
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { PublicationDetailsComponent } from './publication-details/publication-d
   ],
   imports: [
     CommonModule,
-    PublicationsRoutingModule
+    PublicationsRoutingModule,
+    ButtonModule
   ]
 })
 export class PublicationsModule { }
