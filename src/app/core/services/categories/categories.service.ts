172aa6a35b5d23bcc38f7cb6fc4eac775ddae926
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-07-14 10:12:33 +0100

    latest changes

--- src/app/core/services/categories/categories.service.ts
+++ src/app/core/services/categories/categories.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Category } from 'app/models/Category';
+import { AbstractServiceService } from '../abstract-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriesService {
+export class CategoriesService extends AbstractServiceService<Category> {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    super('category', http);
+}
+
 }
--- src/app/modules/participant/events/add-event/add-event.component.ts
+++ src/app/modules/participant/events/add-event/add-event.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { CategoriesService } from 'app/core/services/categories/categories.service';
 import { EventsService } from 'app/core/services/events/events.service';
+import { LocationService } from 'app/core/services/location/location.service';
 import { Category } from 'app/models/Category';
 import { Event } from 'app/models/Event';
+import { Location } from 'app/models/Location';
 import { Type, Type2LabelMapping } from 'app/models/Type';
 import { User } from 'app/models/User';
 import { Visibility, Visibility2LabelMapping } from 'app/models/Visibility';
@@ -17,6 +20,8 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 export class AddEventComponent {
   title: string;
   eventForm!: FormGroup;
+  categories: Category[];
+  locations: Location[];
   public type2LabelMapping = Type2LabelMapping;
   public visibility2LabelMapping = Visibility2LabelMapping;
   public types = Object.values(Type).filter(
@@ -32,10 +37,14 @@ export class AddEventComponent {
       private eventsService: EventsService,
       private messageService: MessageService,
       private confirmationService: ConfirmationService,
-      private keycloakService: KeycloakService
+      private keycloakService: KeycloakService,
+      private categoriesService: CategoriesService,
+      private locationService: LocationService
   ) {
       console.log(this.visibilities);
       console.log(Visibility2LabelMapping[Visibility.PRIVATE]);
+      this.categoriesService.getAll().subscribe((categories)=>{this.categories = categories;});
+      this.locationService.getAll().subscribe((locations)=> {this.locations = locations;});
       this.eventForm = this.fb.group({
           name: [''],
           description: [''],
@@ -69,6 +78,8 @@ export class AddEventComponent {
           this.keycloakService.loadUserProfile().then(u => {
             const organizer: User = {id: u.id};
             let event : Event = new Event();
+            let category: Category = new Category();
+            let location: Location = new Location();
             event.name = this.eventForm.value.content;
             event.dateDebutEvent = this.eventForm.value.dateDebut;
             event.dateFinEvent = this.eventForm.value.dateFinEvent;
@@ -84,10 +95,13 @@ export class AddEventComponent {
             event.locationName = this.eventForm.value.locationName;
             event.rue = this.eventForm.value.rue;
             event.zipCode = this.eventForm.value.zipCode;
-            
-            // this.eventsService.save(event).subscribe((data) => {
-            //   this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Publication created successfully' });
-            // });
+            event.location = this.eventForm.value.location;
+            event.category  = this.eventForm.value.category;
+             this.eventsService.save(event).subscribe((data) => {
+              console.log(event);
+               this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Event created successfully' });
+             });
+             
           });
       }
   }
